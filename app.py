39e193e9037df1538318490e2d95114dad74cdce
Marcin Hoinka <mhoinka@gmail.com>
2022-03-03 23:59:19 +0100

    code formatting

--- app.py
+++ app.py
@@ -6,27 +6,28 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=loggin
 
 api_base_url = "http://127.0.0.1:8000"
 
+
 def get_response():
     logging.info(f'Sending GET request to: {api_base_url}')
     response = requests.get(api_base_url)
     logging.info(f'Got response status code: {response.status_code}')
     logging.info(f'Got response body: {response.json()}')
 
+
 def create_user():
     logging.info(f'Sending POST request')
     new_user = {"firstname": "Hugh", "lastname": "Gass", "age": "35"}
 
-    response = requests.post(api_base_url+"/users/", json=new_user)
+    response = requests.post(api_base_url + "/users/", json=new_user)
     logging.info(f'Got response status code: {response.status_code}')
     # logging.info(f'Got response body: {response.json()}')
 #     TODO get and log response if app1 will return generated user_id
 
 
 def test_fake_users():
-    logging.info(f'Sending GET request')
-
     for id in range(1, 7):
-        response = requests.get(api_base_url+"/users/"+str(id))
+        logging.info(f'Sending GET request')
+        response = requests.get(api_base_url + "/users/" + str(id))
         logging.info(f'Got response status code: {response.status_code}')
         time.sleep(2)
 
